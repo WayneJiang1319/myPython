6d3d220ed2f09a01f15389837c214b50bcd3693b
WayneJiang1319 <jiangwen1319@hotmail.com>
2019-03-14 23:00:49 +0800

    20190314 python study

---
+++ 0314.py
@@ -0,0 +1,15 @@
+print('i am leraning python')
+# this is my python testing code
+print("i am learning \n python")
+# python has three kinds of annotations
+
+# python注意必须缩进一致
+if True:
+    print('TRUE')
+else:
+    print('False')
+
+# 多行语句：如果语句很长，可以使用反斜杠来实现多行语句
+
+
+
