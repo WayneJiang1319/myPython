196ae37923756a348a6deeec5b380892f959a160
WayneJiang1319 <jiangwen1319@hotmail.com>
2019-03-16 14:42:57 +0800

    update

--- 0314.py
+++ 0314.py
@@ -3,13 +3,19 @@ print('i am leraning python')
 print("i am learning \n python")
 # python has three kinds of annotations
 
-# python注意必须缩进一致
+# 1.python注意必须缩进一致
 if True:
     print('TRUE')
 else:
     print('False')
 
 # 多行语句：如果语句很长，可以使用反斜杠来实现多行语句
+
+# python中数字有四种类型，分别为整数、布尔型、浮点数和复数
+# int整数：python只有int，没有long
+# bool 布尔，比如true
+# float浮点数
+# complex复数
 
 
 
