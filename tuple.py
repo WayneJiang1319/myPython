0f2473ab45532cf2f3796e81d9a465c9153dd0ab
WayneJiang1319 <jiangwen1319@hotmail.com>
2019-03-17 18:50:31 +0800

    20190317: tuple

---
+++ tuple.py
@@ -0,0 +1,18 @@
+tuple0=('google','runoob','1997')
+list=['aaa','nnn',2]
+print(tuple)
+tup1=()
+tup2=(50)
+print(type(tup2))
+tup3=(50,)
+print(type(tup3))
+
+del tup2
+#print(tup2)
+
+print(len(tup3))
+
+print(max(tup3))
+print(min(tup3))
+tuple1=tuple(list)
+print(tuple1)
