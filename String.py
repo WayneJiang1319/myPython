e356446251b83c72a98dbe67d72f432b01f2ac0b
WayneJiang1319 <jiangwen1319@hotmail.com>
2019-03-17 16:08:34 +0800

    20190316: python interpreter

---
+++ String.py
@@ -0,0 +1,13 @@
+import  _string
+print('----------------------python转义字符----------------------')
+print('\'')#输出单引号
+print('\"')# 输出双引号
+print('\a')
+print('\b')# 回车
+print('this\00078') # 输出空格
+print('this is \n JIm')# 换行
+print('this is \r x')# 回车
+print('-------------------------python字符串格式化----------------')
+print('my name %s,今年%d 岁！' %('Jim',30))
+
+print(_string.capitalize('abc'))
