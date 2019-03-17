3de917a0e33ec1dd4ea97193127259ccd50ef100
WayneJiang1319 <jiangwen1319@hotmail.com>
2019-03-17 15:48:22 +0800

    20190317 ： number

---
+++ number.py
@@ -0,0 +1,24 @@
+import math
+import random
+print('---------------------python数字类型转换--------------------------')
+a=20
+print(float(a))
+b=21.1
+print(int(b))
+print(complex(a))
+print(complex(a,b))
+
+print('--------------------python数学函数-------------------------------')
+
+c=-20
+print(abs(c))
+print(math.ceil(b))
+print(math.ceil(1))
+print(math.fabs(c))
+print(math.floor(4.1))
+
+print('--------------------随机数函数-------------------------------------')
+print(random.uniform(1,20))
+
+print('--------------------三角函数---------------------------------------')
+print(math.sin(math.pi/6))
