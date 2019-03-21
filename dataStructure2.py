2f726a68455a9200ce016796f5ff4ce1c6292714
WayneJiang1319 <jiangwen1319@hotmail.com>
2019-03-22 07:17:03 +0800

    20190322: dataStructure3

--- dataStructure2.py
+++ dataStructure2.py
@@ -71,4 +71,78 @@ print([x*y for x in vec1 for y in vec2])
 print([vec1[i]*vec2[i] for i in range(len(vec1))])
 
 # 嵌套列表分析
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+print(matrix)
 
+print([[row[i] for row in matrix] for i in range(4)])
+
+
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+
+print(transposed)
+
+# del 语句
+
+a=[-1,1,66,25,333,333,1234.5]
+del a[0]
+print(a)
+
+del a[2:4]
+print(a)
+
+del a[:]
+print(a)
+
+
+# tuple () 元组在输出时总是有括号的，以便表达正确的嵌套结构，在输入时可能没有括号，但是一般是必须要有括号的。
+t=(122,222,"ajajh")
+print(t[0])
+print(t)
+
+# 集合 set
+# 集合是一个无序不重复元素的集，基本功能关系包含关系测试和消除重复元素
+
+basket = {'apple','apple','orange'}
+print(basket)
+
+print('orange' in basket)
+
+print('tea' in basket)
+
+# 字典 键值对
+
+tel = {'a':123,'b':333}
+print(tel)
+
+tel['c']=1254
+print(tel)
+
+print(tel['a'])
+
+print(tel.keys())
+
+print(sorted(tel.keys()))
+
+# 构造dict
+print(dict([('sape',4139),('guido',4127),('jack',4098)]))
+
+# 遍历字典
+# 可以使用items()同时解读出来
+dict1={'a':123,'b':23,'c':333,}
+for k,v in dict1.items():
+    print(k,v)
+# 遍历 序列 list 索引和对于值可以使用enumerate()
+for i ,v in enumerate(['aa','bb','cc']):
+    print(i,v)
+
+# 同时遍历两个或者多个序列，可以使用zip组合
+questions = ['name','hobay','color']
+answers = ['jiangwen','programming','black']
+for q,a in zip(questions,answers):
+    print("what is your {0}? it is {1}".format(q,a))
+# 反向遍历一个序列，首先指定序列，然后reversed()函数
+for i in reversed(range(1,10,2)):
+    print(i)
+# 字母顺序或者个数顺序来遍历一个序列，使用sorted()函数
