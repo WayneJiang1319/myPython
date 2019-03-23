6da20f81dd1a6860cb949363c8d273430de5a338
WayneJiang1319 <jiangwen1319@hotmail.com>
2019-03-23 20:30:28 +0800

    20190323: file method

---
+++ fileMethod.py
@@ -0,0 +1,15 @@
+# open(file,mode='r')方法
+# file 对象，使用open方法来打开，常见方法
+# file.close()：关闭文件
+# # file.flush()：刷新文件内部缓存
+# file.fileno()
+# file.isatty()
+# file.next()
+# file.read()
+# file.readline()
+# file.readlines()
+# file.seek(offset,from_what)
+# file.tell()
+# file.tuncate()
+# file.write()
+# file.writelines()
